0dead6595411aa0edb2c6b3a359066e077297e6b
juhilxbhatt <juhil.k.bhatt@student.uts.edu.au>
2024-11-24 16:01:58 +1100

    template Added & Working

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/download" , methods=['GET', 'POST'])
+def download():
+    return render_template('download.html')
+
+if __name__ == "__main__":
+    app.run()
